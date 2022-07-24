1046ff9cb34bea9ff8f07f83c5ace567e382600b
Freewind <nowindlee@gmail.com>
2022-07-24 17:29:12 +0800

    html-iframe-content-window-on-load-demo

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-Html IFrame onload Demo
+Html IFrame ContentWindow on Load Demo
 =======================
+
+1. iframe load事件，每次都会发生
+2. iframe content window的load事件，只会发生一次
 
 ```
 open index.html
--- index.js
+++ index.js
@@ -1,4 +1,38 @@
-const main = document.getElementById('main');
-main.onload = () => {
-  document.getElementById('message').innerHTML += '<div>iframe is loaded</div>';
-};
+const mainIFrame = document.getElementById('main');
+let button = document.getElementById('button');
+
+button.addEventListener('click', () => {
+    mainIFrame.src = './inner3.html';
+})
+
+mainIFrame.addEventListener('load', (event) => {
+    console.log("### iframe.load", event.target);
+})
+
+mainIFrame.contentWindow.addEventListener('load', (event) => {
+    console.log("### contentWindow.load", event.target.body.innerHTML);
+})
+
+
+setTimeout(() => {
+
+    mainIFrame.src = './inner2.html'
+}, 3000)
+
+// console.log("### mainIFrame?.contentWindow", mainIFrame?.contentWindow.document);
+//
+// setTimeout(() => {
+//     // mainIFrame?.contentWindow.document.addEventListener('DOMContentLoaded', (event) => {
+//     //     console.log('DOM fully loaded and parsed');
+//     // });
+//     mainIFrame.contentWindow.document.onreadystatechange = function(e)
+//     {
+//         console.log('### document.readyState', document.readyState)
+//         // if (document.readyState === 'loading')
+//         // {
+//         //     //dom is ready, window.onload fires later
+//         // }
+//     };
+// }, 0)
+//
+//
